05466ba92c09bcfad15b0308900a42988bf1116b
ChristianFontes <christianfontes82@gmail.com>
2016-05-26 08:32:42 -0430

    New model

---
+++ api/controllers/SeriesTodayController.js
@@ -0,0 +1,11 @@
+/**
+ * SeriesTodayController
+ *
+ * @description :: Server-side logic for managing seriestodays
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/SeriesToday.js
@@ -0,0 +1,30 @@
+/**
+ * SeriesToday.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	owners: {
+		collection: 'user',
+		via: 'SeriesToday'
+    },
+    mySerie: {
+      	model: 'json'
+    },
+    serieId: {
+    	type: 'string',
+    	unique: true
+    },
+    date: {
+    	type: 'date'
+    },
+    read: {
+    	type: 'boolean'
+    }
+  }
+};
+
