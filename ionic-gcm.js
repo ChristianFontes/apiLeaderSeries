fc713286c57e084878eda446c83912306306692b
ChristianFontes <christianfontes82@gmail.com>
2016-05-25 20:12:18 -0430

    Ready

--- ionic-gcm.js
+++ ionic-gcm.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
 var gcm = require('node-gcm');
  
 var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 var server = app.listen(3000, function(){
 	console.log('server is running');
